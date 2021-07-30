64a4662c2d78db95d17d15dc0e65dadcbcce68e7
Abdur Rehman <ww.abdurrehman@gmail.com>
2021-07-30 11:08:18 +0500

    add swagger

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const Joi = require('@hapi/joi');
-
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json")
 
 app.use(express.json());
 
@@ -10,9 +10,13 @@ app.get('/', (req,res) => {
     res.send('swagger ui');
 })
 
+var options = {
+    swaggerOptions: {
+      authAction :{ JWT: {name: "JWT", schema: {type: "apiKey", in: "header", name: "Authorization", description: ""}, value: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Ind3dy5tYWxpY2tyZWhtYW5AZ21haWwuY29tIiwidXNlcklkIjoiNjBmZTc2NmY0ODhmOTEyMzg0ODQ0ODFiIiwiaWF0IjoxNjI3NTM5MzE4fQ.wOXu9PrGrgMDN4OvJg6vOccO1U7SbTYCrGjn3C3Cn1A"} }
+    }
+  };
 
-
-
+  app.use('/swagger',swaggerUi.serve,swaggerUi.setup(swaggerDocument,options));
 
 
 
